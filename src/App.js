6fb1bb5e6c22b67415c83c9ef6fe1ee144381cbc
kunal khairnar <kunalkhairnar61@gmail.com>
2024-04-03 00:23:29 +0530

    Making apps error proof

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,16 +7,36 @@ function App() {
 
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(false)
-  
-  //making api request to get the data inside the component 
-  async function fetchMovieHandler(){
-    try{ 
-      setIsLoading(true)
+  const [isError, setIsError] = useState(null)  
+  const [retrying, setRetrying] = useState(false)
 
+  useEffect(()=>{
+    const id = setInterval(()=>{
+      if(!retrying){
+        fetchMovieHandler()
+      }
+    },5000)
+    return ()=>clearInterval(id)
+  },[retrying])
+
+  //making api request to get the data inside the component 
+   const fetchMovieHandler= async() =>{
+
+    setIsLoading(true)
+    setIsError(null)
+
+    try{ 
+      
     const response = await fetch('https://swapi.dev/api/films/')
     
+
+      if(!response.ok){
+        throw new Error('Something went wrong ...retrying !')
+      }
+
       const data = await response.json()
-      
+
+
         const transformedMovies = data.results.map((movieData)=>{
           return {
             id: movieData.episode_id,
@@ -27,10 +47,41 @@ function App() {
         })
 
        setMovies(transformedMovies)
-       setIsLoading(false)
       } catch(error){
-        console.log('handle the error in the  block',error)
+        setIsError(error.message)
+        setRetrying(true)
       }
+      setIsLoading(false)
+
+  }
+
+  const cancelRetrying =()=>{
+    setIsError(false)
+     setRetrying(false)
+     
+  }
+
+  //error handling and data passing throgw the component and loading content 
+  let content = <p>Found no moveis...</p>
+
+  if(movies.length > 0){
+    content = <MoviesList movies={movies} />
+  }
+
+  if(movies.length === 0){
+    content = <p>Data is not found</p>
+  }
+
+  if(isLoading){
+    content = <p className='loader'>Loading......</p>
+  }
+  
+  if (isError) {
+    content = (
+      <p>
+        {isError} <button onClick={cancelRetrying}>cancel</button>
+      </p>
+    );
   }
 
   return (
@@ -39,9 +90,11 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <h1>Data is not found</h1>}
+        {content}
+        {/* {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !isError && <h1>Data is not found</h1>}
         {isLoading && <p className='loader'></p>}
+        {!isLoading && isError && <p>{isError}</p>} */}
       </section>
     </React.Fragment>
   );
